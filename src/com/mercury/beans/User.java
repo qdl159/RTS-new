2a9f49895ec02c56cf3fe53b5a9ca0693e533155
nieyan0723 <nieyan0723@gmail.com>
2015-12-03 15:04:49 -0500

    MD5 hash

--- src/com/mercury/beans/User.java
+++ src/com/mercury/beans/User.java
@@ -1,5 +1,6 @@
 package com.mercury.beans;
 
+import java.security.MessageDigest;
 import java.util.Random;
 
 public class User {
@@ -203,7 +204,21 @@ public class User {
 		this.role = role;
 	}
 
-
+	public String MD5Hashing(String password)throws Exception{
+		MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        byte byteData[] = md.digest();
+        
+        StringBuffer hexString = new StringBuffer();
+    	for (int i=0;i<byteData.length;i++) {
+    		String hex=Integer.toHexString(0xff & byteData[i]);
+   	     	if(hex.length()==1) hexString.append('0');
+   	     	hexString.append(hex);
+    	}
+		
+		return hexString.toString();
+		
+		}
 	
 	
 	
